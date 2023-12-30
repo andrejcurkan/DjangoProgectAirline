6b855a844beceabd887cc8ff28125ed8b9c72d36
andrej <andrejcurkan@gmail.com>
2023-12-30 15:19:11 +0200

    Create database Airoport, Flight and create simple template

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Flight, Airoport
+from .models import Flight, Airoport, Passenger
 
 # Register your models here.
 admin.site.register(Airoport)
 admin.site.register(Flight)
+admin.site.register(Passenger)
